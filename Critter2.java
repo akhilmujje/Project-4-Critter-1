13ae18cfe29f4005ee051eb28d2cc7581969def9
akhilmujje <akhil.mujje@utexas.edu>
2016-10-16 15:50:06 -0500

    added headers to these classes

--- Critter2.java
+++ Critter2.java
@@ -1,3 +1,15 @@
+/* CRITTERS Craig.java
+ * EE422C Project 4 submission by 
+ * Devisriram Akhil Mujje
+ * 16470
+ * Sriram Ravula
+ * sr39533
+ * 16475
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+
 package assignment4;
 
 public class Critter2 extends Critter {
