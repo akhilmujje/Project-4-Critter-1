58d893f15203ab78e05f35fc7ee86f38c177b938
Sriram Ravula <sriram.ravula@utexas.edu>
2016-10-17 19:36:41 -0500

    Updated header

--- Critter4.java
+++ Critter4.java
@@ -1,11 +1,12 @@
-/* CRITTERS Craig.java
+/* CRITTERS Critter4.java
  * EE422C Project 4 submission by 
  * Devisriram Akhil Mujje
  * 16470
  * Sriram Ravula
  * sr39533
  * 16475
- * Slip days used: <0>
+ * Slip days used: 0
+ * Git URL: https://github.com/akhilmujje/Project-4-Critter-1.git
  * Fall 2016
  */
 
